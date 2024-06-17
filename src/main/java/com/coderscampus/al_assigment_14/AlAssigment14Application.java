1f088d70a09291ad56ca08341dbc7ed581609549
alfuguo <137233519+alfuguo@users.noreply.github.com>
2024-06-17 12:09:39 -0400

    Started

---
+++ src/main/java/com/coderscampus/al_assigment_14/AlAssigment14Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.al_assigment_14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlAssigment14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlAssigment14Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/coderscampus/al_assigment_14/AlAssigment14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.al_assigment_14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlAssigment14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
